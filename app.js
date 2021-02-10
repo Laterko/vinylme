f198351dcefe5ecb117a007b378c3bdcdc7650c2
Piotr <piotrjgladysz@gmail.com>
2021-02-10 20:13:19 +0100

    app update

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 var createError = require('http-errors');
 var express = require('express');
+var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+var mongodbConfig = require("./config/mongodb.config.json");
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+mongoose.connect(mongodbConfig.connection, mongodbConfig.options,
+  () => console.log('connected to DB'));
 
-var app = express();
+app.use(logger('dev'));
+app.use(express.json()); //send json post request
+app.use(express.urlencoded({ extended: true })); //wbudowany bodyparser -> true
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//const User = require('../model/user.schema');
+//middle do routes
+//app.use(require('./routes/index.routes'));
+//app.use(require('./routes/users.routes'));
+const usersRoutes = require('./routes/users.routes');
+app.use('/signup', usersRoutes);
+
+const indexRoutes = require('./routes/index.routes');
+app.use('/', indexRoutes);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
